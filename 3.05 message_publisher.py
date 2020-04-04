4fb0cd7dd4043d74911ae51486334b6223ed3fb5
nimitbdave <nimitbdave@gmail.com>
2020-04-04 13:36:02 -0700

    Update 3.05 message_publisher.py

--- 3.05 message_publisher.py
+++ 3.05 message_publisher.py
@@ -9,14 +9,14 @@ from random import random
 
 rospy.init_node('message_publisher')
 
-pub = rospy.Publisher('complex', Complex)
+pub = rospy.Publisher('complex', Complex)                     # publisher, which calls Complex
 
 rate = rospy.Rate(2)
 
 while not rospy.is_shutdown():
-  msg = Complex()
-  msg.real = random()
-  msg.imaginary = random()
+  msg = Complex()                                             # calls message, Complex()
+  msg.real = random()                                         # when real, random
+  msg.imaginary = random()                                    # when imaginary, random
 
-  pub.publish(msg)
+  pub.publish(msg)                                            # publish
   rate.sleep()
