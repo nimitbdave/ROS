c2a91093c0d129806bc205f673b2620ee73eed1c
nimitbdave <nimitbdave@gmail.com>
2020-04-06 11:58:19 -0700

    Update 4.04 service_client.py

--- 4.04 service_client.py
+++ 4.04 service_client.py
@@ -9,12 +9,23 @@ import sys
 
 rospy.init_node('service_client')
 
-rospy.wait_for_service('word_count')                                    #  First, we wait for the service to be advertised by the server:
+rospy.wait_for_service('word_count')                                    # First, we wait for the service to be advertised by the server:
+                                                                        # If we try to use the service before it’s advertised, 
+                                                                        # the call will fail with an exception.
+    
+                                                                        # This is a major difference between topics and services. 
+                                                                        # We can subscribe to topics that are not yet advertised, 
+                                                                        # but we can only use advertised services. 
+                                                                        
+word_counter = rospy.ServiceProxy('word_count', WordCount)              # Once the service is advertised, 
+                                                                        # we can set up a local proxy for it:
 
-word_counter = rospy.ServiceProxy('word_count', WordCount)
+words = ' '.join(sys.argv[1:])                                          # We need to specify the name of the service (word_count)
+                                                                        # and the type (WordCount). 
+                                                                       
 
-words = ' '.join(sys.argv[1:])
-
-word_count = word_counter(words)
+word_count = word_counter(words)                                        # This will allow us to use word_counter 
+                                                                        # like a local function that, when called, 
+                                                                        # will actually make the service call for us:
 
 print words, '->', word_count.count
